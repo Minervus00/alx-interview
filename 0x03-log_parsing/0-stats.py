05255ba788adef111a2cd82a63591526705327d2
Minervus00 <fayouedraogo@protonmail.com>
2023-04-13 20:54:48 +0000

    log_parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,62 @@
+#!usr/bin/python3
+""" Module for log parsing function """
+import re
+import signal
+
+
+def parse_line(line: str, file_size: int):
+    """Records code and file size if the line has the good format"""
+    pattern = r"^(([0-9]{1,3}\.){3}[0-9]{1,3}) - "
+    pattern += r"\[[0-9]{4}(-[0-9]{2}){2} ([0-9]{2}:){2}[0-9]{2}\.[0-9]{6}\] "
+    pattern += r"\"GET /projects/260 HTTP/1\.1\" ([0-5]{3}) (\d+)$"
+    # print(pattern)
+
+    res = re.fullmatch(pattern, line)
+    if not res:
+        print("no_good")
+        return
+
+    # print("good")
+    code = res.group(5)
+    size = int(res.group(6))
+    # print(code)
+    # print(size)
+    status[code] += 1
+    file_size += size
+    return file_size
+
+
+def print_records():
+    """Prints the records in the required format"""
+    print(f"File size: {file_size}")
+    for code, val in status.items():
+        print(f"{code}: {val}")
+
+
+def signal_handler(signum, frame):
+    print_records()
+    raise KeyboardInterrupt
+
+
+status = dict(
+    (val, 0) for val in [
+        '200', '301', '400', '401', '403', '404', '405', '500'
+    ]
+)
+
+file_size = 0
+
+signal.signal(signal.SIGINT, signal_handler)
+
+itr = 0
+while True:
+    line = input()
+    # print(line)
+    res = parse_line(line, file_size)
+    if res:
+        itr += 1
+        file_size = res
+
+    if itr == 10:
+        itr = 0
+        print_records()
