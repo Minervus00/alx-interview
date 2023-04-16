56a06b4cb8e3e427f5b6489564ac8dacd920b214
Minervus00 <fayouedraogo@protonmail.com>
2023-04-16 11:08:27 +0000

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,44 @@
 #!/usr/bin/python3
-"""log parsing"""
+"""
+Log parsing
+"""
 import sys
 
 
-# def parse_line(line, file_size):
-#     """Records code and file size if the line has the good format"""
-#     pattern = r"^(([0-9]{1,3}\.){3}[0-9]{1,3}) - "
-#  pattern += r"\[[0-9]{4}(-[0-9]{2}){2} ([0-9]{2}:){2}[0-9]{2}\.[0-9]{6}\] "
-#     pattern += r"\"GET /projects/260 HTTP/1\.1\" ([0-5]{3}) (\d+)\n$"
-#     # print(pattern)
-
-#     res = re.fullmatch(pattern, line)
-#     if not res:
-#         # print("no_good")
-#         return
-
-#     code = res.group(5)
-#     size = int(res.group(6))
-#     # print(code)
-#     # print(size)
-#     status[code] += 1
-#     file_size += size
-#     return file_size
-
-
-def print_records():
-    """Prints the records in the required format"""
+def print_metrics(file_size, status_codes):
+    """
+    Print metrics
+    """
     print("File size: {}".format(file_size))
-    for code, val in status.items():
-        print("{}: {}".format(code, val))
+    codes_sorted = sorted(status_codes.keys())
+    for code in codes_sorted:
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
+
+codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+file_size_total = 0
+count = 0
 
 if __name__ == "__main__":
-    status = dict(
-        (val, 0) for val in [
-            '200', '301', '400', '401', '403', '404', '405', '500'
-        ]
-    )
-
-    file_size = 0
-
     try:
-        itr = 0
         for line in sys.stdin:
-            line = line.split()
             try:
-                code = line[-2]
-                if code in status:
-                    status[code] += 1
-                size = int(line[-1])
-                file_size += size
-            except Exception as ex:
+                status_code = line.split()[-2]
+                if status_code in codes_count.keys():
+                    codes_count[status_code] += 1
+                # Grab file size
+                file_size = int(line.split()[-1])
+                file_size_total += file_size
+            except Exception:
                 pass
-
-            itr += 1
-            if itr == 10:
-                itr = 0
-                print_records()
-
-    except KeyboardInterrupt as kb:
-        print_records()
+            # print metrics if 10 lines have been read
+            count += 1
+            if count == 10:
+                print_metrics(file_size_total, codes_count)
+                count = 0
+    except KeyboardInterrupt:
+        print_metrics(file_size_total, codes_count)
         raise
-    print_records()
+    print_metrics(file_size_total, codes_count)
