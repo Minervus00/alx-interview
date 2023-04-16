e95f1b4f6a25e2135209a83ce4c5efd015ba38eb
Minervus00 <fayouedraogo@protonmail.com>
2023-04-16 11:13:10 +0000

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
+"""log parsing"""
 import sys
 
 
-def print_metrics(file_size, status_codes):
-    """
-    Print metrics
-    """
+def print_records(file_size, status):
+    """Prints the records in the required format"""
     print("File size: {}".format(file_size))
-    codes_sorted = sorted(status_codes.keys())
-    for code in codes_sorted:
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    for code, val in status.items():
+        print("{}: {}".format(code, val))
 
-
-codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
-file_size_total = 0
-count = 0
 
 if __name__ == "__main__":
+    status = dict(
+        (val, 0) for val in [
+            '200', '301', '400', '401', '403', '404', '405', '500'
+        ]
+    )
+
+    file_size = 0
+
     try:
+        itr = 0
         for line in sys.stdin:
+            line = line.split()
             try:
-                status_code = line.split()[-2]
-                if status_code in codes_count.keys():
-                    codes_count[status_code] += 1
-                # Grab file size
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
-            except Exception:
+                code = line[-2]
+                if code in status:
+                    status[code] += 1
+                size = int(line[-1])
+                file_size += size
+            except Exception as ex:
                 pass
-            # print metrics if 10 lines have been read
-            count += 1
-            if count == 10:
-                print_metrics(file_size_total, codes_count)
-                count = 0
-    except KeyboardInterrupt:
-        print_metrics(file_size_total, codes_count)
+
+            itr += 1
+            if itr == 10:
+                itr = 0
+                print_records(file_size, status)
+
+    except KeyboardInterrupt as kb:
+        print_records(file_size, status)
         raise
-    print_metrics(file_size_total, codes_count)
+    print_records(file_size, status)
